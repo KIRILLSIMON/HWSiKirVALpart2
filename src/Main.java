1fcf13ace09eaa99acdc177f6d20bf8d4b3ca293
sikir <sikir2022@gmail.com>
2022-11-10 14:13:49 +0300

    Probuyu 2y tranzit.Gotovtes prinimat rannye DZ.

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("zadacha1");
+       /* byte Num1=0;
+        short Num2=1;
+        int Num3=2;
+        long Num4=3;
+        System.out.println("Для типа-byte  -значение переменной равно" + Num1);
+        System.out.println("Для типа-short  -значение переменной равно" +  Num2);
+        System.out.println("Для типа-int  -значение переменной равно" +  Num3);
+        System.out.println("Для типа-long  -значение переменной равно" +  Num4);
+        */System.out.println("Zadacha2");
+        /*double VAR1=27.12;
+        int VAR2= 569;
+        double VAR3=2.786;
+        long VAR4= -159;
+        int VAR5 =67;
+        System.out.println("Значение переменной равно- для VAR с индексом1:"+" "+VAR1);
+        System.out.println("Значение переменной равно- для VAR с индексом2:"+" "+VAR2);
+        System.out.println("Значение переменной равно- для VAR с индексом3:"+" "+ VAR3);
+        System.out.println("Значение переменной равно- для VAR с индексом4:"+" "+ VAR4);
+        System.out.println("Значение переменной равно- для VAR с индексом5:"+" "+VAR5);*/
+       System.out.println("Zadacha3");
+        /*int LP=23;
+       int AS=27;
+       int EA=30;
+       int BuyListsAmount=480;
+       int ListsForOnePiople = BuyListsAmount/(LP+AS+EA);
+       System.out.println(ListsForOnePiople);*/
+        System.out.println("Zadacha4");
+        /*int MoshnostApparataVodnuMinutu=8;
+        int timeInMinute=1;
+        int ProductNumber=timeInMinute*MoshnostApparataVodnuMinutu;
+        System.out.println("Mашина выпустила бутылок количеством"+" "+ ProductNumber+" за период в "+timeInMinute );*/
+        /*int MAVoM2=8;
+        int tIM2=20;
+        int PN2 =MAVoM2*tIM2;
+        System.out.println("Mашина выпустила бутылок количеством"+" "+ PN2+" за период в " + tIM2);*/
+        /*int MAVoM3=8;
+        int tIM3=24*60;
+        int PN3 =MAVoM3*tIM3;
+        System.out.println("Mашина выпустила бутылок количеством"+" "+ PN3+" за период в " + tIM3);*/
+        /*int MAVoM4=8;
+        int tIM4=24*60*3;
+        int PN4 =MAVoM4*tIM4;
+        System.out.println("Mашина выпустила бутылок количеством"+" "+ PN4+" за период в " + tIM4);*/
+        /*int MAVoM5=8;
+        int tIM5=24*60*30;
+        int PN5 =MAVoM5*tIM5;
+        System.out.println("Mашина выпустила бутылок количеством"+" "+ PN5+" за период в " + tIM5);
+        */
+       /* System.out.println("Zadacha5");
+        int TotalPaintInBoutlesAmount= 120;
+        int ExpensesBoutleToOneClassroom=6;
+        int AmCl=TotalPaintInBoutlesAmount/ExpensesBoutleToOneClassroom;
+        System.out.println(AmCl);
+        int ConsumedWhiteToOneCabinet=2;
+        int ConsumedBrownToOneCabinet=4;
+        System.out.println(ConsumedWhiteToOneCabinet);
+        System.out.println(ConsumedBrownToOneCabinet);
+        int NecAmWPB=AmCl*ConsumedWhiteToOneCabinet;
+        int NecAmBPB=AmCl*ConsumedBrownToOneCabinet;
+        System.out.println(NecAmWPB);
+        System.out.println(NecAmBPB);
+        System.out.println("В шк. из " +" "+AmCl+"классов"+" "+"взять б-к к-ки:"+" white-"+NecAmWPB+"brown"+NecAmBPB)*/
+        System.out.println("Zadacha6");
+         double BananaWeightInGr =80;
+         double MilkWeightPer100mlInGr=105;
+         double OneBricketIceCreamWeightInGr=100;
+         double OneEggWeightInGr=70;
+         double AmountBananas=5;
+         double AmountMilk=2;
+         double AmountBricketIceCream=2;
+         double AmountEggs=4;
+         double sojratoBananas= BananaWeightInGr*AmountBananas;
+         double vylokanoMilk=MilkWeightPer100mlInGr*AmountMilk;
+         double slopanoIC=OneBricketIceCreamWeightInGr*AmountBricketIceCream;
+         double zaglochenoYaitsav=OneEggWeightInGr*AmountEggs;
+         double totalWeight= sojratoBananas+vylokanoMilk+slopanoIC+zaglochenoYaitsav;
+        System.out.println(sojratoBananas);
+        System.out.println(vylokanoMilk);
+        System.out.println(slopanoIC);
+        System.out.println(zaglochenoYaitsav);
+        System.out.println(totalWeight);
+        double TWinKG=totalWeight/1000;
+        System.out.println(TWinKG);
+        System.out.println("Какое безобразие :(  (спортивная жирность взлетела аж на "+TWinKG+"КГ!-уже НЕ стройняшка!");
+
+
+        System.out.println("от Симонова К.О. реш-е 1й, 2й, 3й, 4й, 5й, 6й задач по 2й ч.урока Переменные");
+        }
+
+}
